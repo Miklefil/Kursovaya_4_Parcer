d72b44572bdc0b3b46e7da816fea37e5f3edcc3f
twist <twistysmig@mail.ru>
2023-03-26 21:08:22 +0300

    Added class coonector with description, realised main and utils

--- classes/connector.py
+++ classes/connector.py
@@ -1,5 +1,6 @@
 import json
 
+
 class Connector:
     """
     Класс коннектор к файлу, обязательно файл должен быть в json формате
@@ -7,58 +8,71 @@ class Connector:
     внешнего деградации
     """
     __data_file = None
-    @property
-    def data_file(self):
-        pass
 
-    @data_file.setter
-    def data_file(self, value):
-        # тут должен быть код для установки файла
+    def __init__(self, file_path: str):
+        self.__data_file = file_path
         self.__connect()
 
-    def __connect(self):
-        """
-        Проверка на существование файла с данными и
-        создание его при необходимости
-        Также проверить на деградацию и возбудить исключение
-        если файл потерял актуальность в структуре данных
-        """
-        pass
+    @property
+    def data_file(self) -> str:
+        return self.__data_file
 
-    def insert(self, data):
-        """
-        Запись данных в файл с сохранением структуры и исходных данных
-        """
-        pass
+    @data_file.setter
+    def data_file(self, value: str) -> None:
+        """Установка файла"""
+        self.__data_file = value
+        self.__connect()
 
-    def select(self, query):
+    def __connect(self) -> None:
+        """Перезаписывает файл на [] или создает новый и записывает []"""
+        with open(self.__data_file, 'w') as file:
+            json.dump([], file)
+
+    def insert(self, data: list) -> None:
+        """Запись данных в файл с сохранением структуры и исходных данных"""
+
+        with open(self.__data_file, 'r', encoding='UTF-8') as file:
+            data_json = json.load(file)
+
+        with open(self.__data_file, 'w', encoding='UTF-8') as file:
+            json.dump(data_json + data, file, indent=2, ensure_ascii=False)
+
+    def select(self, query: dict) -> list:
         """
         Выбор данных из файла с применением фильтрации
-        query содержит словарь, в котором ключ это поле для
-        фильтрации, а значение это искомое значение, например:
-        {'price': 1000}, должно отфильтровать данные по полю price
-        и вернуть все строки, в которых цена 1000
+        query- словаря, в котором ключ - это поле для
+        фильтрации, а значение - это искомое значение, если
+        передан пустой словарь, возвращает все данные файла
         """
-        pass
+        result = []
+        with open(self.__data_file) as f:
+            data = json.load(f)
 
-    def delete(self, query):
+        if not query:
+            return data
+
+        for item in data:
+            if all(item.get(key) == value for key, value in query.items()):
+                result.append(item)
+
+        return result
+
+    def delete(self, query: dict) -> list | None:
         """
-        Удаление записей из файла, которые соответствуют запрос,
+        Удаление записей из файла, которые соответствуют запросу,
         как в методе select. Если в query передан пустой словарь, то
         функция удаления не сработает
         """
-        pass
+        if not query:
+            return
 
+        with open(self.__data_file) as f:
+            data = json.load(f)
 
-if __name__ == '__main__':
-    df = Connector('df.json')
+        result = []
+        for item in data:
+            if not all(item.get(key) == value for key, value in query.items()):
+                result.append(item)
 
-    data_for_file = {'id': 1, 'title': 'tet'}
-
-    df.insert(data_for_file)
-    data_from_file = df.select(dict())
-    assert data_from_file == [data_for_file]
-
-    df.delete({'id':1})
-    data_from_file = df.select(dict())
-    assert data_from_file == []
+        with open(self.__data_file, 'w') as file:
+            json.dump(result, file)
